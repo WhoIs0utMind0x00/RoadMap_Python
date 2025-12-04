1002c00ba7080213578868eed2a9687df0c19ae7
AbibPerez <abibiDe1000@outlook.com>
2025-12-03 20:57:01 -0600

    Funciones & Alcance

--- #02 - Funciones & Alcance.py
+++ #02 - Funciones & Alcance.py
@@ -1,15 +1,28 @@
-def uno_cien():
-    for n in range(1, 101):
-        print(n)
-def multiplos():
-    for n in range(1, 101):
-        if n % 1 == 0:
-            print(n)
-        if n % 3 == 0:
-            print(f"{n} es múltiplo de 3")
-        if n % 5 == 0:
-            print(f"{n} es múltiplo de 5")
-        if n % 5 == 0 and n % 3 == 0:
-            print(f"{n} es múltiplo de 3 y de 5")
+def contar_apariciones(a, b):
+        conteo_tres = 0
+        conteo_cinco = 0
+        conteo_ambos = 0
+        conteo_numero = 0
+        # Itera en el rango numérico 1 - 100
+        for n in range(1, 101):
+            if n % 3 == 0 and n % 5 == 0:   # Filtra múltiplos en común de 3 y 5
+                print(a + b)                # Imprime el valor de los parámetros [a] y [b] concatenados
+                conteo_ambos += 1
+            elif n % 3 == 0:                # Filtra múltiplos de 3
+                print(a)                    # Imprime el valor del parámetro [a]
+                conteo_tres += 1
+            elif n % 5 == 0:                # Filtra múltiplos de 5
+                print(b)                    # Imprime el valor del parámetro [b]
+                conteo_cinco += 1
+            else:
+                print(n)                    # Imprime los números que no cumplen ninguna función anterior
+                conteo_numero += 1
+        # Imprime estadísticas
+        print("Cantidad total de caracteres impresos:", conteo_tres + conteo_cinco + conteo_ambos + conteo_numero)
+        print(f"Cantidad de veces que se imprimió {a}:", conteo_tres)
+        print(f"Cantidad de veces que se imprimió {b}:", conteo_cinco)
+        print(f"Cantidad de veces que se imprimió {a + b}:", conteo_ambos)
+        return conteo_numero                # Devuelve el conteo de los números impresos
 
-multiplos()
+apariciones = contar_apariciones("X", "O")
+print("Cantidad de veces que se imprimió un número:", apariciones)
